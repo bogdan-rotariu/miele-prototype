821e7d8b7f2a9ee6134509d0b7d2c7ca9a207268
Bogdan Rotariu <bogdanrotariu@Bogdans-MacBook-Pro.local>
2022-01-25 13:09:57 +0200

    feat: add Overlay, PullDown components, build storybook

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,2 +1,3 @@
 export * from './program-list-topbar'
 export * from './program-list-footer'
+export * from './pulldown'
---
+++ src/components/molecules/pulldown/pulldown.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import classNames from 'classnames'
+import { node } from 'prop-types'
+import { Button, Overlay } from '../../atoms'
+import styles from './pulldown.module.scss'
+
+export const Pulldown = ({ children }) => {
+    const [isActive, setIsActive] = useState(false)
+    return (
+        <Overlay isActive={isActive}>
+            <div
+                className={classNames(styles.pulldown, {
+                    [styles.pulldownActive]: isActive,
+                })}
+            >
+                <div className={styles.body}>{children}</div>
+                <footer className={styles.footer}>
+                    <Button onClick={() => setIsActive(false)}>Close</Button>
+                </footer>
+                <button
+                    className={styles.handle}
+                    onClick={() => setIsActive(!isActive)}
+                ></button>
+            </div>
+        </Overlay>
+    )
+}
+
+Pulldown.propTypes = {
+    children: node,
+}
